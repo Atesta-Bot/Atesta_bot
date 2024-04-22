143e6855855ced4e348ae6233e2504f5fa5d51bb
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-04-22 13:53:06 -0600

    Router & Chakra UI implemented

--- frontend/attesta-bot-dapp/src/components/pages/Home.tsx
+++ frontend/attesta-bot-dapp/src/components/pages/Home.tsx
@@ -1,7 +1,11 @@
+import { Button, Flex } from "@chakra-ui/react";
+
 function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <Flex></Flex>
+      <Button> Go to DApp</Button>
     </div>
   );
 }
--- frontend/attesta-bot-dapp/src/main.tsx
+++ frontend/attesta-bot-dapp/src/main.tsx
@@ -1,10 +1,18 @@
+import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const container = document.getElementById("root");
+const root = createRoot(container as HTMLElement);
+
+root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
