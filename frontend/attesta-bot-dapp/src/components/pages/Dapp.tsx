a0704adbf66d094106d412810518d81e53265969
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-04-22 14:36:25 -0600

    navbar dapp implementation 1

--- frontend/attesta-bot-dapp/src/components/pages/Dapp.tsx
+++ frontend/attesta-bot-dapp/src/components/pages/Dapp.tsx
@@ -1,9 +1,23 @@
+import { Button, Link, VStack, Flex } from "@chakra-ui/react";
+
 function DappPage() {
   return (
-    <>
-      <h1>Attesta Bot Dapp</h1>
-      <p>Here you will find the Dapp for the Attesta Bot project.</p>
-    </>
+    <Flex width="100%" flexDir="row" bgColor="#EAEAEA">
+      <VStack as="nav" spacing={4} align="start" p={5} bg="white">
+        <Link to="/">
+          <Button variant="ghost">Logo</Button>
+        </Link>
+        <Link to="/pending-tickets">
+          <Button variant="ghost">Pending Tickets</Button>
+        </Link>
+        <Link to="/whitelist">
+          <Button variant="ghost">Whitelist</Button>
+        </Link>
+        <Link to="/history">
+          <Button variant="ghost">History</Button>
+        </Link>
+      </VStack>
+    </Flex>
   );
 }
 
--- frontend/attesta-bot-dapp/src/components/pages/Home.tsx
+++ frontend/attesta-bot-dapp/src/components/pages/Home.tsx
@@ -1,11 +1,17 @@
-import { Button, Flex } from "@chakra-ui/react";
+import { Button, Flex, Heading, Image } from "@chakra-ui/react";
+import logo from "../../assets/logos/Logo 1 Blue Attesta Bot.svg";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Home</h1>
-      <Flex></Flex>
-      <Button> Go to DApp</Button>
+      <Flex flexDir="column">
+        <Image src={logo} alt="logo" />
+        <Heading> WELCOME TO TETILLAS-BOT!!</Heading>
+      </Flex>
+      <Button onClick={() => navigate("/dapp")}> Go to DApp</Button>
     </div>
   );
 }
