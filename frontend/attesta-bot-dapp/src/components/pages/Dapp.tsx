99a6f2629f72f7ef497a64b7213bb4de9321939c
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-04-22 13:00:19 -0600

    Router Dom

---
+++ frontend/attesta-bot-dapp/src/components/pages/Dapp.tsx
@@ -0,0 +1,10 @@
+function DappPage() {
+  return (
+    <>
+      <h1>Attesta Bot Dapp</h1>
+      <p>Here you will find the Dapp for the Attesta Bot project.</p>
+    </>
+  );
+}
+
+export default DappPage;
