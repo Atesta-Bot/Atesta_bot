e0ec5e3d952887f1a40f861e6839b8916c006a80
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-04-23 14:28:49 -0600

    Dapp Layout & Pending Ticket Layout

--- frontend/attesta-bot-dapp/src/components/pages/Dapp.tsx
+++ frontend/attesta-bot-dapp/src/components/pages/Dapp.tsx
@@ -1,22 +1,481 @@
-import { Button, Link, VStack, Flex } from "@chakra-ui/react";
+import {
+  Flex,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Image,
+  Heading,
+  Button,
+  Text,
+  Box,
+  Select,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  IconButton,
+} from "@chakra-ui/react";
+import "@fontsource/inconsolata";
+import "@fontsource/iceland";
+import logo from "../../assets/logos/Logo 1 Blue Attesta Bot.svg";
 
 function DappPage() {
   return (
-    <Flex width="100%" flexDir="row" bgColor="#EAEAEA">
-      <VStack as="nav" spacing={4} align="start" p={5} bg="white">
-        <Link to="/">
-          <Button variant="ghost">Logo</Button>
-        </Link>
-        <Link to="/pending-tickets">
-          <Button variant="ghost">Pending Tickets</Button>
-        </Link>
-        <Link to="/whitelist">
-          <Button variant="ghost">Whitelist</Button>
-        </Link>
-        <Link to="/history">
-          <Button variant="ghost">History</Button>
-        </Link>
-      </VStack>
+    <Flex width="100%" flexDir="row" bgColor="#EAEAEA" h="100%">
+      <Tabs w="100%" h="100vh" orientation="vertical">
+        <TabList
+          bgColor="white"
+          style={{
+            fontFamily: "Inconsolata",
+          }}
+          w="22rem"
+        >
+          <Flex
+            flexDir="column"
+            style={{ padding: "1rem", justifyContent: "center" }}
+            justifyItems="center"
+          >
+            <Image alignSelf="center" boxSize="4rem" src={logo} alt="Logo" />
+            <p
+              style={{
+                color: "#45A5FF",
+                fontFamily: "Iceland",
+                fontSize: "36px",
+                fontWeight: "400",
+                textAlign: "center",
+              }}
+            >
+              Attesta Bot
+            </p>
+          </Flex>
+
+          <Tab _selected={{ color: "#45A5FF", bgColor: "white" }}>
+            Pending Tickets
+          </Tab>
+          <Tab _selected={{ color: "#45A5FF", bgColor: "white" }}>
+            Whitelist
+          </Tab>
+          <Tab _selected={{ color: "#45A5FF", bgColor: "white" }}>History</Tab>
+        </TabList>
+
+        <TabPanels w="100%" h="100%">
+          <TabPanel width="100%" h="100%">
+            <Flex flexDir="column" w="100%" h="100%">
+              <Flex
+                borderRadius="8px"
+                padding="2rem"
+                justify="space-between"
+                bgColor="#45A5FF"
+              >
+                <Box>
+                  <Heading color="white" fontSize="4rem" fontFamily="Iceland">
+                    Welcome to Attesta Bot
+                  </Heading>
+                  <Text fontSize="1.5rem" color="white">
+                    Review and pay your pending tickets{" "}
+                  </Text>
+                </Box>
+
+                <Button color="white" variant={"outline"} ml="8rem">
+                  Connect Wallet
+                </Button>
+              </Flex>
+              {/* <Flex
+                p="1rem"
+                borderRadius="8px"
+                mt="2rem"
+                h="6rem"
+                bgColor="white"
+                justify="space-between"
+              >
+                <Flex flexDir="column" ml="1rem">
+                  <Text
+                    fontFamily="Inconsolata"
+                    color="#1E1E1E"
+                    fontSize="12px"
+                    fontWeight="light"
+                  >
+                    UID
+                  </Text>
+                  <Text
+                    fontFamily="Inconsolata"
+                    color="#45A6FF"
+                    fontSize="18px"
+                  >
+                    ...h6f5
+                  </Text>
+                </Flex>
+                <Flex flexDir="column" ml="1rem">
+                  <Text
+                    fontFamily="Inconsolata"
+                    color="#1E1E1E"
+                    fontSize="12px"
+                    fontWeight="light"
+                  >
+                    From
+                  </Text>
+                  <Text
+                    fontFamily="Inconsolata"
+                    color="#45A6FF"
+                    fontSize="18px"
+                  >
+                    0xVat0...h6f5
+                  </Text>
+                </Flex>
+                <Flex flexDir="column" ml="1rem">
+                  <Text
+                    fontFamily="Inconsolata"
+                    color="#1E1E1E"
+                    fontSize="12px"
+                    fontWeight="light"
+                  >
+                    Description
+                  </Text>
+                  <Text
+                    fontFamily="Inconsolata"
+                    color="#1E1E1E"
+                    fontSize="18px"
+                  >
+                    Some Chicken Wings for the team...
+                  </Text>
+                </Flex>
+                <Flex flexDir="column" ml="1rem">
+                  <Text
+                    fontFamily="Inconsolata"
+                    color="#1E1E1E"
+                    fontSize="12px"
+                    fontWeight="light"
+                  >
+                    Amount
+                  </Text>
+                  <Text
+                    fontFamily="Inconsolata"
+                    color="#1E1E1E"
+                    fontSize="18px"
+                    alignSelf="center"
+                  >
+                    186.25
+                  </Text>
+                </Flex>
+                <Select
+                  ml="1rem"
+                  alignSelf="center"
+                  w="5rem"
+                  placeholder="USDT"
+                >
+                  <option value="ETH">ETH</option>
+                  <option value="OPT">OPT</option>
+                  <option value="ARB">ARB</option>
+                </Select>
+                <Button ml="1rem" colorScheme="blue">
+                  PAY
+                </Button>
+              </Flex> */}
+
+              <Accordion
+                borderRadius="8px"
+                mt="2rem"
+                bgColor="white"
+                allowToggle
+                p="1rem"
+              >
+                <AccordionItem borderColor="transparent">
+                  <Flex w="100%" justify="space-between">
+                    <Flex flexDir="column" ml="1rem">
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#1E1E1E"
+                        fontSize="12px"
+                        fontWeight="light"
+                      >
+                        UID
+                      </Text>
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#45A6FF"
+                        fontSize="18px"
+                      >
+                        ...h6f5
+                      </Text>
+                    </Flex>
+                    <Flex flexDir="column" ml="1rem">
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#1E1E1E"
+                        fontSize="12px"
+                        fontWeight="light"
+                      >
+                        From
+                      </Text>
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#45A6FF"
+                        fontSize="18px"
+                      >
+                        0xVat0...h6f5
+                      </Text>
+                    </Flex>
+                    <Flex flexDir="column" ml="1rem">
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#1E1E1E"
+                        fontSize="12px"
+                        fontWeight="light"
+                      >
+                        Description
+                      </Text>
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#1E1E1E"
+                        fontSize="18px"
+                      >
+                        Some Chicken Wings for the team...
+                      </Text>
+                    </Flex>
+                    <Flex flexDir="column" ml="1rem">
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#1E1E1E"
+                        fontSize="12px"
+                        fontWeight="light"
+                      >
+                        Amount
+                      </Text>
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#1E1E1E"
+                        fontSize="18px"
+                        alignSelf="center"
+                      >
+                        186.25
+                      </Text>
+                    </Flex>
+                    <Select
+                      ml="1rem"
+                      alignSelf="center"
+                      w="5rem"
+                      placeholder="USDT"
+                    >
+                      <option value="ETH">ETH</option>
+                      <option value="OPT">OPT</option>
+                      <option value="ARB">ARB</option>
+                    </Select>
+                    <Button ml="1rem" colorScheme="blue">
+                      PAY
+                    </Button>
+                    <AccordionButton w="4rem">
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </Flex>
+                  <AccordionPanel pb={4}>
+                    <Flex justify="space-evenly">
+                      <Flex flexDir="column" ml="1rem">
+                        <Text
+                          fontFamily="Inconsolata"
+                          color="#1E1E1E"
+                          fontSize="12px"
+                          fontWeight="light"
+                        >
+                          Organization
+                        </Text>
+                        <Text
+                          fontFamily="Inconsolata"
+                          color="#45A6FF"
+                          fontSize="18px"
+                        >
+                          Attesta Bot
+                        </Text>
+                      </Flex>
+                      <Flex flexDir="column" ml="1rem">
+                        <Text
+                          fontFamily="Inconsolata"
+                          color="#1E1E1E"
+                          fontSize="12px"
+                          fontWeight="light"
+                        >
+                          Date
+                        </Text>
+                        <Text
+                          fontFamily="Inconsolata"
+                          color="#45A6FF"
+                          fontSize="18px"
+                        >
+                          14/05/2021
+                        </Text>
+                      </Flex>
+                      <Button>View Ticket</Button>
+                      <Button ml="1rem" colorScheme="orange">
+                        Reject
+                      </Button>
+                    </Flex>
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+              <Accordion
+                borderRadius="8px"
+                mt="2rem"
+                bgColor="white"
+                allowToggle
+                p="1rem"
+              >
+                <AccordionItem borderColor="transparent">
+                  <Flex w="100%" justify="space-between">
+                    <Flex flexDir="column" ml="1rem">
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#1E1E1E"
+                        fontSize="12px"
+                        fontWeight="light"
+                      >
+                        UID
+                      </Text>
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#45A6FF"
+                        fontSize="18px"
+                      >
+                        ...h6f5
+                      </Text>
+                    </Flex>
+                    <Flex flexDir="column" ml="1rem">
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#1E1E1E"
+                        fontSize="12px"
+                        fontWeight="light"
+                      >
+                        From
+                      </Text>
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#45A6FF"
+                        fontSize="18px"
+                      >
+                        0xVat0...h6f5
+                      </Text>
+                    </Flex>
+                    <Flex flexDir="column" ml="1rem">
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#1E1E1E"
+                        fontSize="12px"
+                        fontWeight="light"
+                      >
+                        Description
+                      </Text>
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#1E1E1E"
+                        fontSize="18px"
+                      >
+                        Some Chicken Wings for the team...
+                      </Text>
+                    </Flex>
+                    <Flex flexDir="column" ml="1rem">
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#1E1E1E"
+                        fontSize="12px"
+                        fontWeight="light"
+                      >
+                        Amount
+                      </Text>
+                      <Text
+                        fontFamily="Inconsolata"
+                        color="#1E1E1E"
+                        fontSize="18px"
+                        alignSelf="center"
+                      >
+                        186.25
+                      </Text>
+                    </Flex>
+                    <Select
+                      ml="1rem"
+                      alignSelf="center"
+                      w="5rem"
+                      placeholder="USDT"
+                    >
+                      <option value="ETH">ETH</option>
+                      <option value="OPT">OPT</option>
+                      <option value="ARB">ARB</option>
+                    </Select>
+                    <Button ml="1rem" colorScheme="blue">
+                      PAY
+                    </Button>
+                    <AccordionButton w="4rem">
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </Flex>
+                  <AccordionPanel pb={4}>
+                    <Flex justify="space-evenly">
+                      <Flex flexDir="column" ml="1rem">
+                        <Text
+                          fontFamily="Inconsolata"
+                          color="#1E1E1E"
+                          fontSize="12px"
+                          fontWeight="light"
+                        >
+                          Organization
+                        </Text>
+                        <Text
+                          fontFamily="Inconsolata"
+                          color="#45A6FF"
+                          fontSize="18px"
+                        >
+                          Attesta Bot
+                        </Text>
+                      </Flex>
+                      <Flex flexDir="column" ml="1rem">
+                        <Text
+                          fontFamily="Inconsolata"
+                          color="#1E1E1E"
+                          fontSize="12px"
+                          fontWeight="light"
+                        >
+                          Date
+                        </Text>
+                        <Text
+                          fontFamily="Inconsolata"
+                          color="#45A6FF"
+                          fontSize="18px"
+                        >
+                          14/05/2021
+                        </Text>
+                      </Flex>
+                      <Button>View Ticket</Button>
+                      <Button ml="1rem" colorScheme="orange">
+                        Reject
+                      </Button>
+                    </Flex>
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+
+              <Flex
+                borderRadius="8px"
+                mt="2rem"
+                h="6rem"
+                bgColor="white"
+              ></Flex>
+              <Flex
+                borderRadius="8px"
+                mt="2rem"
+                h="6rem"
+                bgColor="white"
+              ></Flex>
+            </Flex>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Flex>
   );
 }
