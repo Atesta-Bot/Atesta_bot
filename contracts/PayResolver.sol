41c944d65ed6fe46641a4b10d34c17ead317e12d
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-05-08 13:57:32 -0600

    Comments for contracts address

--- contracts/PayResolver.sol
+++ contracts/PayResolver.sol
@@ -10,7 +10,11 @@ import { SchemaResolver } from "../SchemaResolver.sol";
 
 import { IEAS, Attestation } from "../../IEAS.sol";
 
+//Deployed Schema
+//https://sepolia.easscan.org/schema/view/0xae4b749f83e0b4b890cc7d88a6cfad54fc1ff2a89ee8b1db38e8367469356a3e
 
+// Sepolia ETH SAFEPaying-ResolverWithToken contract
+// 0xcD0B16Ef43eD4213bd3FFAB27170101F1b237f17
 
 /// @title PayingResolver with token
 /// @notice A sample schema resolver that pays to an address provided in the attestation data .
@@ -21,6 +25,8 @@ contract PayingResolver is SchemaResolver {
      using SafeERC20 for IERC20;
 
     constructor(IEAS eas,  IERC20 targetToken) SchemaResolver(eas) {
+//         Sepolia ETH FToken MUSDT mintable
+// 0x88c2A1f01a866a2f30e947d51F7c58f8b1dce57B
        _targetToken = targetToken;
     }
 
