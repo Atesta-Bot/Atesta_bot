f1946b7d2f0335c46967d6cd3dbc2bdb00a1032d
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-06-27 16:05:24 -0600

    transfer makeAttestation to paymodal

--- frontend/attesta-bot-dapp/src/components/organisms/Attestation-Ticket.tsx
+++ frontend/attesta-bot-dapp/src/components/organisms/Attestation-Ticket.tsx
@@ -9,9 +9,7 @@ import {
   Select,
   Text,
 } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
-import { SchemaEncoder, EAS } from "@ethereum-attestation-service/eas-sdk";
-import { ethers } from "ethers";
+
 import PaymentModal from "./PayModal";
 
 interface AttestationTicketProps {
@@ -39,52 +37,6 @@ const AttestationTicket: React.FC<AttestationTicketProps> = ({
 }) => {
   const useNavigate = () => {
     window.open(imageLink, "_blank");
-  };
-
-  const makeAttestation = async () => {
-    // base sepolia eas 0x4200000000000000000000000000000000000021
-    // sepolia  0xC2679fBD37d54388Ce493F1DB75320D236e1815e
-    const eas = new EAS("0x4200000000000000000000000000000000000021");
-    // Gets a default provider (in production use something else like infura/alchemy)
-    const provider = new ethers.BrowserProvider(window.ethereum);
-    const signer = await provider.getSigner();
-
-    eas.connect(signer);
-
-    // Initialize SchemaEncoder with the schema string
-    const schemaEncoder = new SchemaEncoder(
-      "string granteeAddress, uint256 amount, string comments"
-    );
-    const encodedData = schemaEncoder.encodeData([
-      {
-        name: "granteeAddress",
-        value: attester,
-        type: "string",
-      },
-      { name: "amount", value: BigInt(amount), type: "uint256" },
-      { name: "comments", value: "no comments", type: "string" },
-    ]);
-
-    //eth sepolia schema: 0xa51bb919ff8236abd4689317eaeb03fcaae3defb487623eebf08284264af1218
-
-    //Base schema UID
-    const schemaUID =
-      "0xa51bb919ff8236abd4689317eaeb03fcaae3defb487623eebf08284264af1218";
-
-    const transaction = await eas.attest({
-      schema: schemaUID,
-      data: {
-        recipient: attester,
-        expirationTime: BigInt(0),
-        revocable: true, // Be aware that if your schema is not revocable, this MUST be false
-        refUID: fullUid,
-        data: encodedData,
-      },
-    });
-
-    const newAttestationUID = await transaction.wait();
-
-    console.log("New attestation UID:", newAttestationUID);
   };
 
   return (
@@ -132,7 +84,12 @@ const AttestationTicket: React.FC<AttestationTicketProps> = ({
             >
               Description
             </Text>
-            <Text fontFamily="Inconsolata" color="#1E1E1E" fontSize="18px">
+            <Text
+              maxW="22rem"
+              fontFamily="Inconsolata"
+              color="#1E1E1E"
+              fontSize="18px"
+            >
               {description}
             </Text>
           </Flex>
@@ -160,7 +117,11 @@ const AttestationTicket: React.FC<AttestationTicketProps> = ({
             <option value="ARB">ARB</option>
           </Select>
           <Button ml="1rem" colorScheme="blue">
-            <PaymentModal makeAttestation={makeAttestation} />
+            <PaymentModal
+              fullUid={fullUid}
+              attester={attester}
+              attestedAmount={amount}
+            />
           </Button>
           <AccordionButton w="4rem">
             <AccordionIcon />
--- frontend/attesta-bot-dapp/src/components/organisms/PayModal.tsx
+++ frontend/attesta-bot-dapp/src/components/organisms/PayModal.tsx
@@ -10,14 +10,74 @@ import {
   ModalCloseButton,
   Button,
   Input,
+  Text,
 } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import { SchemaEncoder, EAS } from "@ethereum-attestation-service/eas-sdk";
+import { ethers } from "ethers";
 
-const PaymentModal = ({ makeAttestation }) => {
+interface PaymentModalProps {
+  attester: string;
+  fullUid: string;
+  attestedAmount: string;
+}
+
+const PaymentModal: React.FC<PaymentModalProps> = ({
+  attester,
+  fullUid,
+  attestedAmount,
+}) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [amount, setAmount] = useState("");
 
+  const makeAttestation = async () => {
+    // base sepolia eas 0x4200000000000000000000000000000000000021
+    // sepolia  0xC2679fBD37d54388Ce493F1DB75320D236e1815e
+    const eas = new EAS("0x4200000000000000000000000000000000000021");
+    // Gets a default provider (in production use something else like infura/alchemy)
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+
+    eas.connect(signer);
+
+    // Initialize SchemaEncoder with the schema string
+    const schemaEncoder = new SchemaEncoder(
+      "string granteeAddress, uint256 amount, string comments"
+    );
+    const encodedData = schemaEncoder.encodeData([
+      {
+        name: "granteeAddress",
+        value: attester,
+        type: "string",
+      },
+      { name: "amount", value: BigInt(amount), type: "uint256" },
+      { name: "comments", value: "no comments", type: "string" },
+    ]);
+
+    //eth sepolia schema: 0xa51bb919ff8236abd4689317eaeb03fcaae3defb487623eebf08284264af1218
+
+    //Base schema UID
+    const schemaUID =
+      "0xa51bb919ff8236abd4689317eaeb03fcaae3defb487623eebf08284264af1218";
+
+    const transaction = await eas.attest({
+      schema: schemaUID,
+      data: {
+        recipient: attester,
+        expirationTime: BigInt(0),
+        revocable: true, // Be aware that if your schema is not revocable, this MUST be false
+        refUID: fullUid,
+        data: encodedData,
+      },
+    });
+
+    const newAttestationUID = await transaction.wait();
+
+    console.log("New attestation UID:", newAttestationUID);
+  };
+
   const handlePay = async () => {
-    await makeAttestation(amount); // Assuming makeAttestation can directly accept the amount
+    makeAttestation;
     onClose(); // Close the modal after payment
   };
 
@@ -31,6 +91,7 @@ const PaymentModal = ({ makeAttestation }) => {
           <ModalHeader fontFamily="Inconsolata">Payment</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
+            <Text>Total Amount:{attestedAmount}</Text>
             <Input
               placeholder="Enter payment amount"
               value={amount}
